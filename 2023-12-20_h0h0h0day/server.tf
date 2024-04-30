980953ac276323a3d67a99dab70e30cebab7f873
Olivier Bilodeau <olivier@bottomlesspit.org>
2024-04-30 15:59:04 -0400

    h0h0h0day 2023: updated large instance slug we used

--- 2023-12-20_h0h0h0day/server.tf
+++ 2023-12-20_h0h0h0day/server.tf
@@ -7,7 +7,7 @@ resource "digitalocean_droplet" "ctfd" {
   # initial size (smallest cpu-optimized)
   size     = "s-2vcpu-4gb"
   # production size
-  #size     = "c-32"
+  #size     = "c-16-intel"
   resize_disk = false
   ssh_keys = [data.digitalocean_ssh_key.server.fingerprint]
 
